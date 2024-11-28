f2d73ee6ea1db1f1f5d0dae0161b0adc5ebe0524
Diksha <dikshasugandhi277@gmail.com>
2024-11-28 14:03:15 +0530

    fourth commit

--- db.js
+++ db.js
@@ -9,12 +9,26 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
-
 });
 
 db.connect(err => {
     if (err) throw err;
     console.log('MySQL Connected...!');
+    
+    const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS schools (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            address VARCHAR(255) NOT NULL,
+            latitude DOUBLE NOT NULL,
+            longitude DOUBLE NOT NULL
+        );
+    `;
+    
+    db.query(createTableQuery, (err, result) => {
+        if (err) throw err;
+        console.log('Schools table is ready!');
+    });
 });
 
 module.exports = db;
