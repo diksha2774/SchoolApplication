b3efb08a80609ae18ec62f53ce88e9a383617f64
Diksha <dikshasugandhi277@gmail.com>
2024-11-28 12:00:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const schoolRoutes = require('./routes/SchoolRoute');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/', schoolRoutes);
+
+const PORT = process.env.PORT || 28171;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
